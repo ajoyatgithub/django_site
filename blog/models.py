6c28533618bce742f71d5a756023fc6a449f108f
Ajoy Oommen <ajoy91ad@gmail.com>
2015-03-21 10:14:39 +0530

    Add Tag and Category. Add __unicode__ methods

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.CharField(max_length=100, db_index=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('p', 'Published'),
@@ -13,3 +27,7 @@ class Post(models.Model):
     created = models.DateTimeField(db_index=True)
     modified= models.DateTimeField()
     tags    = models.ManyToManyField(Tag, null=True, blank=True)
+    category= models.ForeignKey(Category)
+
+    def __unicode__(self):
+        return self.title

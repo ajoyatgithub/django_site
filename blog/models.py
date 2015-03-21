57e32b66929d5f7356477fbbdaedd501cf31b75d
Ajoy Oommen <ajoy91ad@gmail.com>
2015-03-21 10:26:18 +0530

    Register models on admin. Add Meta for Category

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('p', 'Published'),

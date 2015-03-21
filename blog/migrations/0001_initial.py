4ea4380944fd4b15430867cb82b6811cd0adee71
Ajoy Oommen <ajoy91ad@gmail.com>
2015-03-21 10:18:39 +0530

    Add blog and create migrations

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100, db_index=True)),
+                ('slug', models.CharField(max_length=100, db_index=True)),
+                ('parent', models.ForeignKey(blank=True, to='blog.Category', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('slug', models.CharField(max_length=100)),
+                ('body', models.TextField()),
+                ('status', models.CharField(default=b'd', max_length=2)),
+                ('created', models.DateTimeField(db_index=True)),
+                ('modified', models.DateTimeField()),
+                ('category', models.ForeignKey(to='blog.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(to='blog.Tag', null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]

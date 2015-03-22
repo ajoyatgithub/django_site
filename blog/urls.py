e72715a57dd8c4a4317fffe3118ab328c1bd98ff
Ajoy Oommen <ajoy91ad@gmail.com>
2015-03-22 23:42:50 +0530

    Add view methods and map urls to the methods

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.home),
-                       url(r'^(?P<pid>\d+)/(?P<slug>[-\w\d]+)$', views.home),
-                       url(r'^c/(?P<slug>[-\w\d]+)$', views.home),
+                       url(r'^(?P<pid>\d+)/(?P<slug>[-\w\d]+)$', views.post),
+                       url(r'^c/(?P<slug>[-\w\d]+)$', views.category),
                        url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<y>\day+)$',
-                           views.home)
+                           views.archive)
 )
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,34 @@ def home(request):
         'posts': posts,
         'categories': categories
     })
+
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    posts = category.post_set.filter(status='p').order_by('-created')
+    categories = Category.tree()
+    return render(request, 'home.tpl', {
+        'posts': posts,
+        'categories': categories
+    })
+
+def archive(request, year, month=None, day=None):
+    posts = Post.objects.filter(status='p')
+    if year:
+        posts = posts.filter(created__year=year)
+    if month:
+        posts = posts.filter(created__month=month)
+    if day:
+        posts = posts.filter(created__day=day)
+    categories = Category.tree()
+    return render(request, 'home.tpl', {
+        'posts': posts,
+        'categories': categories
+    })
+
+def post(request, pid, slug):
+    post = Post.objects.get(id=pid)
+    categories = Category.tree()
+    return render(request, 'home.tpl', {
+        'post': post,
+        'categories': categories
+    })

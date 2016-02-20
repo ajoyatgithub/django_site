6cc2891508e4560a3ff58cc43925e77904014b5c
Ajoy Oommen <ajoy91ad@gmail.com>
2016-02-20 22:42:19 +0530

    Refer bootstrap in base template

--- blog/urls.py
+++ blog/urls.py
@@ -3,21 +3,19 @@ from django.views.generic import RedirectView
 
 from blog import views
 
-urlpatterns = patterns('',
-                       url(r'^$', views.home),
-                       url(r'^(?P<pid>\d+)/(?P<slug>[-\w\d]+)$', views.post),
-
-                       url(r'^category/(?P<slug>[-\w\d]+)/$', views.category),
-                       url(r'^c/(?P<slug>[-\w\d]+)/$',
-                           RedirectView.as_view(pattern_name='blog.views.category')),
-
-                       url(r'^archives/(?P<year>\d+)/$',
-                           views.archive, name="archives_year"),
-                       url(r'^a/(?P<year>\d+)/$',
-                           RedirectView.as_view(pattern_name='archives_year')),
-
-                       url(r'^archives/(?P<year>\d+)/(?P<month>\d+)/$',
-                           views.archive, name="archives_month"),
-                       url(r'^a/(?P<year>\d+)/(?P<month>\d+)/$',
-                           RedirectView.as_view(pattern_name='archives_month')),
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.home),
+    url(r'^(?P<pid>\d+)/(?P<slug>[-\w\d]+)$', views.post),
+    url(r'^category/(?P<slug>[-\w\d]+)/$', views.category),
+    url(r'^c/(?P<slug>[-\w\d]+)/$',
+        RedirectView.as_view(pattern_name='blog.views.category')),
+    url(r'^archives/(?P<year>\d+)/$',
+        views.archive, name="archives_year"),
+    url(r'^a/(?P<year>\d+)/$',
+        RedirectView.as_view(pattern_name='archives_year')),
+    url(r'^archives/(?P<year>\d+)/(?P<month>\d+)/$',
+        views.archive, name="archives_month"),
+    url(r'^a/(?P<year>\d+)/(?P<month>\d+)/$',
+        RedirectView.as_view(pattern_name='archives_month')),
 )

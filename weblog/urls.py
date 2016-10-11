dc2661cf4287f19042094d3a689d72fcd73318e1
Ajoy Oommen <ajoy91ad@gmail.com>
2016-10-11 18:23:19 +0530

    Add urls for django-markdown

--- weblog/urls.py
+++ weblog/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url('^markdown/', include( 'django_markdown.urls')),
     url('^', include('blog.urls', namespace="blog"))
 ]
